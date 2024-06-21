b463cca6ea3eaeb9ea88932794868df5ded5d077
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2024-06-21 17:49:33 +0300

    Initial Commit

---
+++ imageanon/imageanonApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageanonApp.swift
+//  imageanon
+//
+//  Created by Martin Myhre on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct imageanonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
